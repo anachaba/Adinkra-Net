3e62a3a3347ef9db330edb5365b473c3e82516d4
anach <janumbia334@gmail.com>
2023-05-11 19:15:14 +0000

    update

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from tensorflow import keras
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,make_response
 import numpy as np
 import os
 
-class_names1 = ['Aban','Abe_Dua','Adikrahene_Dua','Adinkrahene','Adwera','Adwo','Agyinduwura','Akoben','Akofena','Akokonan','Akoma','Akoma_Ntoso','Ananse_Ntontan','Ani_Bere','Asase_Ye_Duru','Aya','Bese_Saka','Biribi_Wo_Soro','Bi_Nnka_Bi','Boa_Me_Na_Me_Boa_Wo','Dame_Dame','Denkyem','Dono','Duafe','Dwannimmen','Eban','Epa','Ese_Ne_Tekrema','Fafanto','Fawohudie','Fihankra','Fofo','Funtumfunafu_Denkyem_Funafu','Gye_Nyame','Hwemudua','Hye_Won_Hye','Kae_Me','Kete_Pa','Kintinkantan','Kojo_Baiden','Kontire_Ne_Akwamu','Krado','Kramo_Bone','Kuntinkantan','Kwatakye_Atiko','Mako','Mate_Masie','Mframadan','Mmere_Dane','Mmomudwan','Mmusuyidee','Mpatapo','Mpuannum','Nea_Onnim_No_Sua_A_Ohu','Nea_Ope_Se_Nkrofoo_Ye_Ma_Wo_No-_Ye_Saa_Ara_Ma_won','Nea_Ope_Se_Obedi_Hene','Nkonsonkonson','Nkontim','Nkuma_Kese','Nkyimu','Nkyinkyim','Nnonnowa','Nsaa','Nserewa','Nsoromma','Nyame_Akruma','Nyame_Biribi_Wo_Soro','Nyame_Dua','Nyame_Nnwu_Na_Mawu','Nyame_Nti','Nyame_Ye_Ohene','Nyansapo','Nya_Abotere','Odo_Nyera_Fie_Kwan','Ohene','Ohene_Aniwa','Ohene_Tuo','Ohen_Adwae','Okodee_Mmowere','Okuafo_Pa','Onyakopon_Adom_Nti_Biribiara_Beye_Yie_African_Adinkra_Weddin','Onyakopon_Aniwa','Onyakopon_Ne_Yen_Ntena','Osidan','Osram','Osram_Ne_Nsoromma','Owo_Foro_Adobe','Owuo_Atwedee','Owuo_Kum_Nyame','Pa_Gya','Sankofa','Sepow','Sesa_Woruban','Sunsum','Tabon','Tamfo_Bebre','Tumi_Te_Se_Kosua','Tuo_Ne_Akofena','Wawa_Aba','Wo_Nsa_Da_Mu_A','Wuforo_Dua_Pa_A']
+
+class_names1 = ['Aban','Abe Dua','Adikrahene Dua','Adinkrahene','Adwera','Adwo','Agyinduwura','Akoben','Akofena','Akokonan','Akoma','Akoma Ntoso','Ananse Ntontan','Ani Bere','Asase Ye Duru','Aya','Bese Saka','Biribi Wo Soro','Bi Nnka Bi','Boa Me Na Me Boa Wo','Dame Dame','Denkyem','Dono','Duafe','Dwannimmen','Eban','Epa','Ese Ne Tekrema','Fafanto','Fawohudie','Fihankra','Fofo','Funtumfunafu Denkyem Funafu','Gye Nyame','Hwemudua','Hye Won Hye','Kae Me','Kete Pa','Kintinkantan','Kojo Baiden','Kontire Ne Akwamu','Krado','Kramo Bone','Kuntinkantan','Kwatakye Atiko','Mako','Mate Masie','Mframadan','Mmere Dane','Mmomudwan','Mmusuyidee','Mpatapo','Mpuannum','Nea Onnim No Sua A Ohu','Nea Ope Se Nkrofoo Ye Ma Wo No Ye Saa Ara Ma won','Nea Ope Se Obedi Hene','Nkonsonkonson','Nkontim','Nkuma Kese','Nkyimu','Nkyinkyim','Nnonnowa','Nsaa','Nserewa','Nsoromma','Nyame Akruma','Nyame Biribi Wo Soro','Nyame Dua','Nyame Nnwu Na Mawu','Nyame Nti','Nyame Ye Ohene','Nyansapo','Nya Abotere','Odo Nyera Fie Kwan','Ohene','Ohene Aniwa','Ohene Tuo','Ohen Adwae','Okodee Mmowere','Okuafo Pa','Onyakopon Adom Nti Biribiara Beye Yie African Adinkra Weddin','Onyakopon Aniwa','Onyakopon Ne Yen Ntena','Osidan','Osram','Osram Ne Nsoromma','Owo Foro Adobe','Owuo Atwedee','Owuo Kum Nyame','Pa Gya','Sankofa','Sepow','Sesa Woruban','Sunsum','Tabon','Tamfo Bebre','Tumi Te Se Kosua','Tuo Ne Akofena','Wawa Aba','Wo Nsa Da Mu A','Wuforo Dua Pa A']
 
 
 IMG_SIZE = (224, 224)
@@ -17,7 +18,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    response = make_response(render_template('index.html'))
+    response.headers['ngrok-skip-browser-warning'] = 'true'
+    return response
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -41,4 +44,4 @@ def predict():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
